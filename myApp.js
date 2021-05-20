526ca3b273588a40a94b5e5ae2d557c7d785d643
aniruddh2018 <53439290+aniruddh2018@users.noreply.github.com>
2021-05-20 11:58:56 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+app.get("/json", (req, res) => { let message = ""; (process.env.MESSAGE_STYLE == "uppercase") ? 
+message=message.toUpperCase() : message="Hello json"; res.json({"message": message}); });
 
 
 
